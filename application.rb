027deb34c2026c4577e89ec8f645603b1777617c
razerbeans <razebeans@gmail.com>
2010-04-10 23:06:14 -0500

    Cleaner configuration. Fixed run issue.

--- application.rb
+++ application.rb
@@ -7,12 +7,22 @@ require 'boardwalk'
 
 set :environment, :development
 
+configure do
+  set :server, %w[mongrel webrick]
+  set :port, 3002
+end
+
 configure :development do
   set :s3key, '44CF9590006BF252F707'
   set :s3secret, 'OtxrzxIsfpFjA7SwPzILwy8Bw21TLhquhboDYROV'
-  set :server, %w[mongrel webrick thin]
-  set :port, 3002
+  # set :server, %w[mongrel webrick]
+  # set :port, 3002
   set :logging, true
+  set :show_exceptions, false
+end
+
+configure :production do
+  set :logging, false
   set :show_exceptions, false
 end
 
@@ -20,16 +30,16 @@ end
 # STORAGE_PATH = File.join(Dir.pwd, 'storage')
 # STATIC_PATH = File.expand_path('views/', File.dirname(__FILE__))
 RESOURCE_TYPES = %w[acl torrent]
-# CANNED_ACLS = {
-#     'private' => 0600,
-#     'public-read' => 0644,
-#     'public-read-write' => 0666,
-#     'authenticated-read' => 0640,
-#     'authenticated-read-write' => 0660
-# }
-# READABLE = 0004
-# WRITABLE = 0002
-# READABLE_BY_AUTH = 0040
-# WRITABLE_BY_AUTH = 0020
+CANNED_ACLS = {
+    'private' => 0600,
+    'public-read' => 0644,
+    'public-read-write' => 0666,
+    'authenticated-read' => 0640,
+    'authenticated-read-write' => 0660
+}
+READABLE = 0004
+WRITABLE = 0002
+READABLE_BY_AUTH = 0040
+WRITABLE_BY_AUTH = 0020
 
 Boardwalk.run
