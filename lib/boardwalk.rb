6b5098cee3cfef705bef80a8db2d8746a5651ae2
razerbeans <razebeans@gmail.com>
2010-04-10 23:07:26 -0500

    Cleaned up includes.

--- lib/boardwalk.rb
+++ lib/boardwalk.rb
@@ -1,16 +1,7 @@
-current = File.join(File.dirname(__FILE__))
-# require 'sinatra'
 require 'openssl'
 require 'base64'
 require 'hmac-sha1'
-# if options.webservice
-#   require "#{current}/boardwalk/control_routes.rb"
-# end
-# require "#{current}/boardwalk/mimetypes_hash.rb"
-# # require "#{current}/boardwalk/s3_routes.rb"
-# require "#{current}/boardwalk/s3_service.rb"
-# require "#{current}/boardwalk/models.rb"
-require 'boardwalk/mimetypes_hash'
+require 'boardwalk/mimetypes'
 # require 'boardwalk/s3_routes'
 require 'boardwalk/s3_service'
 require 'boardwalk/models'
